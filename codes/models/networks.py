72e01c72d4d31059f17ecfd6599ce71a1a3e7796
Luca Ranalli <luca.ranalli@unicredit.eu>
2020-04-20 11:20:49 +0200

    Added mixed block, added test tb log

--- codes/models/networks.py
+++ codes/models/networks.py
@@ -1,9 +1,6 @@
 import torch
-import models.archs.SRResNet_arch as SRResNet_arch
 import models.archs.discriminator_vgg_arch as SRGAN_arch
-import models.archs.RRDBNet_arch as RRDBNet_arch
 #import models.archs.EDVR_arch as EDVR_arch
-import models.archs.discriminator_effnet as EffNet_arch
 
 # Generator
 def define_G(opt):
@@ -12,20 +9,24 @@ def define_G(opt):
 
     # image restoration
     if which_model == 'MSRResNet':
+        import models.archs.SRResNet_arch as SRResNet_arch
+
         netG = SRResNet_arch.MSRResNet(in_nc=opt_net['in_nc'], out_nc=opt_net['out_nc'],
                                        nf=opt_net['nf'], nb=opt_net['nb'], upscale=opt_net['scale'])
     elif which_model == 'RRDBNet':
+        import models.archs.RRDBNet_arch as RRDBNet_arch
+
         upsample_type = opt_net.get('upsample_type', 'interpolate')
         netG = RRDBNet_arch.RRDBNet(in_nc=opt_net['in_nc'], out_nc=opt_net['out_nc'],
                                     nf=opt_net['nf'], nb=opt_net['nb'], upscale=opt_net['scale'],
                                     upsample_type=upsample_type)
-    # video restoration
-    elif which_model == 'EDVR':
-        netG = EDVR_arch.EDVR(nf=opt_net['nf'], nframes=opt_net['nframes'],
-                              groups=opt_net['groups'], front_RBs=opt_net['front_RBs'],
-                              back_RBs=opt_net['back_RBs'], center=opt_net['center'],
-                              predeblur=opt_net['predeblur'], HR_in=opt_net['HR_in'],
-                              w_TSA=opt_net['w_TSA'])
+    # # video restoration
+    # elif which_model == 'EDVR':
+    #     netG = EDVR_arch.EDVR(nf=opt_net['nf'], nframes=opt_net['nframes'],
+    #                           groups=opt_net['groups'], front_RBs=opt_net['front_RBs'],
+    #                           back_RBs=opt_net['back_RBs'], center=opt_net['center'],
+    #                           predeblur=opt_net['predeblur'], HR_in=opt_net['HR_in'],
+    #                           w_TSA=opt_net['w_TSA'])
     else:
         raise NotImplementedError('Generator model [{:s}] not recognized'.format(which_model))
 
@@ -40,6 +41,8 @@ def define_D(opt):
     if which_model == 'discriminator_vgg_128':
         netD = SRGAN_arch.Discriminator_VGG_128(in_nc=opt_net['in_nc'], nf=opt_net['nf'])
     elif 'efficientnet' in which_model:
+        import models.archs.discriminator_effnet as EffNet_arch
+
         pretrained_path = opt_net.get('pretrained', None)
         netD = EffNet_arch.Discriminator_EfficientNet(which_model, in_ch=opt_net['in_nc'],
                                                       pretrained=pretrained_path, drop_fc=True)
@@ -67,6 +70,8 @@ def define_F(opt, use_bn=False):
         netF = SRGAN_arch.VGGFeatureExtractor(feature_layer=feature_layer, in_ch=in_ch, use_bn=use_bn,
                                               use_input_norm=use_input_norm, device=device)
     elif 'efficientnet' in which_model:
+        import models.archs.discriminator_effnet as EffNet_arch
+
         pretrained = opt_net['pretrained']
         blocks = opt_net.get('perceptual_blocks', [1,3,5])
         netF = EffNet_arch.EfficientNetFeatureExtractor(which_model, in_ch=in_ch, pretrained=pretrained,
